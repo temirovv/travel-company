caff468f28fe1450c21d3372f3c3d1520989337d
temirovv <temirovv21@gmail.com>
2024-08-31 05:39:04 +0500

    project configured

---
+++ root/settings/development.py
@@ -0,0 +1,9 @@
+from root.settings.base import *
+
+
+load_dotenv(BASE_DIR / '.env/.env.local')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', '*'))
---
+++ root/settings/production.py
@@ -0,0 +1,25 @@
+import os
+
+from root.settings.base import *
+
+
+load_dotenv(BASE_DIR / '.env/.env.local')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_NAME'),
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': os.getenv('PG_HOST'),
+        'PORT': os.getenv('PG_PORT'),
+        "TEST": {
+            "MIRROR": "default",
+        },
+    }
+}
+
