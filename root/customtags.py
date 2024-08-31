f849a0a9c26b34674863b6fbb5fd802c21397ed0
temirovv <temirovv21@gmail.com>
2024-08-31 06:23:00 +0500

    Jazzmin's problem with length_is filter solved

---
+++ root/customtags.py
@@ -0,0 +1,22 @@
+import warnings
+from django.template.library import Library
+
+register = Library()
+
+
+class RemovedInDjango51Warning(PendingDeprecationWarning):
+    pass
+
+
+@register.filter(is_safe=False)
+def length_is(value, arg):
+    """Return a boolean of whether the value's length is the argument."""
+    warnings.warn(
+        "The length_is template filter is deprecated in favor of the length template "
+        "filter and the == operator within an {% if %} tag.",
+        RemovedInDjango51Warning,
+    )
+    try:
+        return len(value) == int(arg)
+    except (ValueError, TypeError):
+        return ""
