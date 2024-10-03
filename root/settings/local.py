81892adb22a3074d038b06acb5b37ac7e35de5e9
temirovv <temirovv21@gmail.com>
2024-10-03 12:26:10 +0500

    local settings updated

--- root/settings/local.py
+++ root/settings/local.py
@@ -5,7 +5,6 @@ load_dotenv(BASE_DIR / '.env/.env.local')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# DEBUG = os.getenv('DEBUG')
-# DEBUG = True
-# ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', '*'))
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+DEBUG = os.getenv('DEBUG')
+
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', '*'))
