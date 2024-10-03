ce69c22ec64fe1c5154a512053f9a6e5e6029b49
temirovv <temirovv21@gmail.com>
2024-10-03 11:40:22 +0500

    users updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView, View
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'users/authentication/register.html'
+    success_url = reverse_lazy('login')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'users/authentication/login.html'
+
+
+class CustomLogoutView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse('home'))
+
+
 ''''
 universal_drf_template
 minio
