f340945cedf4aa1f56475bbb38e786c69037e7d3
temirovv <temirovv21@gmail.com>
2024-10-03 11:40:45 +0500

    this is just notes to learn

--- dkkashdkjhkaj.py
+++ dkkashdkjhkaj.py
@@ -10,3 +10,23 @@
 #
 # for i in Place.objects.filter(name='botirjon'):
 #     print(i)
+
+
+
+
+''''
+universal_drf_template
+minio
+redis
+mongodb
+mysql
+postgres
+celery
+sms(eskiz)
+click, payme, uzum
+elasticsearch
+graphql
+websocket
+github, gitlab (cicd)
+docker compose
+'''
